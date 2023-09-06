902fe2441a03bac88b3b79fe6c56ce4020091867
Carlos Suarez Barrera <carlijavier5@gmail.com>
2023-09-06 19:04:24 -0400

    Bonbon Combo Core base

---
+++ Assets/_Scripts/Turn Based Mechanics/BCC/BonbonObject.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Scriptable Object class for all and every bonbon;
+/// </summary>
+public class BonbonObject : ScriptableObject {
+
+    public readonly string name;
+    public readonly Sprite sprite;
+
+    /// <summary> A description of the bonbon for later use in the UI; </summary>
+    public readonly string description;
+
+    /// <summary> An array of status effects granted by the bonbon; </summary>
+    //public StatusEffect[] statusEffects;
+
+    /// <summary> A dictionary containing the bonbons required to make the bonbon, mapped to the required quantity; </summary>
+    public readonly BonbonObject[] recipe;
+
+    public override bool Equals(object other) {
+        if (other is BonbonObject) {
+            BonbonObject oBonbon = other as BonbonObject;
+            return name == oBonbon.name /*StatusEffect.Equals(statusEffects, oBonbon.statusEffects)*/
+                           && recipe.RecipeEquals(oBonbon.recipe); 
+        } return false;
+    }
+
+    public override int GetHashCode() {
+        return HashCode.Combine(base.GetHashCode(), name, recipe);
+    }
+}
