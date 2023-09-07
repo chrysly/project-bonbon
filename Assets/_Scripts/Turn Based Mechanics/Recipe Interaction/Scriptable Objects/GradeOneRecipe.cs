e3e26ae366e39b0a6cbf4969ea1dc790bb268cea
Carlijavier5 <carlijavier5@gmail.com>
2023-09-06 21:07:25 -0400

    Remove Previous Recipe Interaction Scripts

--- Assets/_Scripts/Turn Based Mechanics/Recipe Interaction/Scriptable Objects/GradeOneRecipe.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Recipe Data/Grade 1")]
-public class GradeOneRecipe : Recipe
-{
-
-}
--- Assets/_Scripts/Turn Based Mechanics/Recipe Interaction/Scriptable Objects/SpecialGradeRecipe.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Recipe Data/Special Grade")]
-public class SpecialGradeRecipe : Recipe
-{
-
-}
