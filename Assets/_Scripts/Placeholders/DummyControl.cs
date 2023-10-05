c69768339a78e9ce52f3e0c66f7feb4152456ba8
Carlijavier5 <carlijavier5@gmail.com>
2023-10-04 20:01:40 -0400

    Quick GameManager Fix

--- Assets/_Scripts/Placeholders/DummyControl.cs
+++ Assets/_Scripts/Placeholders/DummyControl.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class DummyControl : MonoBehaviour {
 
     public void SetCore(int coreIndex) {
-        GameManager.TransitionToCore((GameManager.CoreScene) coreIndex);
+        GameManager.Instance.TransitionToCore((GameManager.CoreScene) coreIndex);
     }
 
     public void SetLevel(int levelIndex) {
-        GameManager.TransitionToLevel(levelIndex);
+        GameManager.Instance.TransitionToLevel(levelIndex);
     }
 }
