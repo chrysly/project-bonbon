da40b61feac518a1b4ec95db28acd3354737ef71
ivu-u <113319454+ivu-u@users.noreply.github.com>
2023-10-10 19:08:21 -0400

    basic layout for event sequencer

--- Assets/_Scripts/EventSequencer/EventSequencer.cs
+++ Assets/_Scripts/EventSequencer/EventSequencer.cs
@@ -1,40 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Yarn.Unity;
 
-public class EventSequencer : MonoBehaviour // not sure if should be monobehabior
+/// <summary>
+/// Event runner --> holds events for each scene, and runs them when conditions are met
+/// </summary>
+public class EventSequencer
 {
+    DialogueRunner _dialogueRunner;
     public List<EventObject> eventSequence;
-    public Transform spawnPoint;    // temporary
+    Queue<EventObject> events = new Queue<EventObject>();
 
-    private int currentEventIndex = 0;
-
-    // where we need to execute events --> eventSequencer.StartEventSequence();
-    public void StartEventSequence()
-    {
-        currentEventIndex = 0;
-        ExecuteNextEvent();
-    }
-
-    private void ExecuteNextEvent()
-    {
-        if (currentEventIndex < eventSequence.Count)
-        {
-            EventObject currentEvent = eventSequence[currentEventIndex];
-
-            // check if you need to load prefab
-            if (currentEvent.prefabToLoad != null)
-            {
-                Instantiate(currentEvent.prefabToLoad, spawnPoint.position, Quaternion.identity);
+    public void CheckForEvents(AIActionValue package) { 
+        // add any events that meet activate conditions to a queue
+        foreach (EventObject ev in eventSequence) { 
+            if (ev.CheckConitions(package)) {
+                events.Enqueue(ev);
             }
-
-            currentEvent.OnTrigger();
-            currentEventIndex++;
         }
-        else
-        {
-            // it's done with all events
-            Debug.Log("No more sequence");
+
+        // run the next event in queue
+        if (events.Count > 0) {
+            EventObject next = events.Dequeue();
+            next.OnTrigger();
         }
     }
 }
