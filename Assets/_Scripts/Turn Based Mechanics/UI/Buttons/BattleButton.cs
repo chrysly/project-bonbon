421a385091132c0ddaa458fe28efd6a565792bc3
clee857 <chrysly0816@gmail.com>
2023-10-12 05:16:32 -0400

    Main Panel UI Setup

--- Assets/_Scripts/Turn Based Mechanics/UI/Buttons/BattleButton.cs
+++ Assets/_Scripts/Turn Based Mechanics/UI/Buttons/BattleButton.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
+using JetBrains.Annotations;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class BattleButton : MonoBehaviour
-{
+public class BattleButton : MonoBehaviour {
+    public Transform targetPoint;
     public virtual void Activate(BattleUIStateMachine stateMachine, float delay) {}
     public void Scale(Vector3 scale, float duration) {
         transform.DOScale(scale, duration);
--- Assets/_Scripts/Turn Based Mechanics/UI/UI State Machine/UIAnimationHandler.cs
+++ Assets/_Scripts/Turn Based Mechanics/UI/UI State Machine/UIAnimationHandler.cs
@@ -11,13 +11,19 @@ public class UIAnimationHandler : MonoBehaviour {
     //Everything should pass through activeUIAction! This is to prevent other animations overlapping
     public IEnumerator activeUIAction = null;
     [SerializeField] private BattleUIStateMachine _stateMachine;
+    [SerializeField] private Canvas mainCanvas;
 
     public void Start() {
         DisableAll();
+        cursor.gameObject.SetActive(false);
+    }
+
+    private void LateUpdate() {
+        mainCanvas.transform.LookAt(Camera.main.transform);
     }
 
     public void DisableAll() {
-        ToggleMainPanel(false);
+        ToggleMainPanel(false, true);
     }
 
     public bool QueueIsEmpty() {
@@ -29,31 +35,48 @@ public class UIAnimationHandler : MonoBehaviour {
     [SerializeField] private CanvasGroup mainPanel;
     [SerializeField] private float mainPanelToggleDuration = 0.3f;
 
+    [SerializeField] private Transform cursor;
+
     [SerializeField] private List<BattleButton> mainPanelButtons;
+    [SerializeField] private List<Transform> decorations;
     [SerializeField] private Vector3 mainPanelButtonScaleVector = new Vector3(1.2f, 1.2f, 1.2f);
     [SerializeField] private float mainPanelButtonScaleDuration = 0.2f;
     [SerializeField] private float mainPanelButtonEmergeDuration = 1f;
     private int mainButtonIndex = -1;
 
-    public void ToggleMainPanel(bool enable) {
+    public void ToggleMainPanel(bool enable, bool force = false) {
         if (QueueIsEmpty()) {
             if (enable) mainPanel.gameObject.SetActive(true);
-            activeUIAction = MainPanelAction(enable);
+            activeUIAction = MainPanelAction(enable, force);
             StartCoroutine(activeUIAction);
         }
     }
-    private IEnumerator MainPanelAction(bool enable) {
+    private IEnumerator MainPanelAction(bool enable, bool force) {
         //mainPanel.DOFade(enable ? 1 : 0, mainPanelToggleDuration); FADE
+        if (!enable) CollapseCursor();
         foreach (BattleButton button in mainPanelButtons) {
             if (enable) {
                 button.gameObject.SetActive(true);
-                button.transform.DOScale(new Vector3(1, 1, 1), mainPanelButtonEmergeDuration);
-                yield return new WaitForSeconds(mainPanelButtonEmergeDuration);
+                button.transform.DOScale(new Vector3(1, 1, 1), force ? 0 : mainPanelButtonEmergeDuration).SetEase(Ease.InOutBounce);
+                if (!force) yield return new WaitForSeconds(mainPanelButtonEmergeDuration);
             }
             else {
-                button.transform.DOScale(new Vector3(0, 0, 0), mainPanelButtonEmergeDuration);
-                yield return new WaitForSeconds(mainPanelButtonEmergeDuration);
+                button.transform.DOScale(new Vector3(0, 0, 0), force ? 0 : mainPanelButtonEmergeDuration);
+                if (!force) yield return new WaitForSeconds(mainPanelButtonEmergeDuration);
                 button.gameObject.SetActive(false);
+            }
+        }
+
+        foreach (Transform decoration in decorations) {
+            if (enable) {
+                //decoration.gameObject.SetActive(true);
+                decoration.transform.DOScale(new Vector3(1, 1, 1), force ? 0 : mainPanelButtonEmergeDuration).SetEase(Ease.InOutBounce);
+                if (!force) yield return new WaitForSeconds(mainPanelButtonEmergeDuration);
+            }
+            else {
+                decoration.transform.DOScale(new Vector3(0, 0, 0), force ? 0 : mainPanelButtonEmergeDuration);
+                if (!force) yield return new WaitForSeconds(mainPanelButtonEmergeDuration);
+                //decoration.gameObject.SetActive(false);
             }
         }
         if (!enable) mainPanel.gameObject.SetActive(false);
@@ -68,6 +91,7 @@ public class UIAnimationHandler : MonoBehaviour {
         }
     }
     private IEnumerator SelectMainPanelButtonAction(bool directionDown) {
+        InitCursor();
         mainButtonIndex = mainButtonIndex == -1 ? 0 : mainButtonIndex;
         if (directionDown) mainButtonIndex = mainButtonIndex >= mainPanelButtons.Count - 1 ? 0 : mainButtonIndex + 1;
         else mainButtonIndex = mainButtonIndex <= 0 ? mainPanelButtons.Count - 1 : mainButtonIndex - 1;
@@ -79,11 +103,29 @@ public class UIAnimationHandler : MonoBehaviour {
                 mainPanelButtons[i].Scale(new Vector3(1, 1, 1), mainPanelButtonScaleDuration);
             }
         }
+        
+        UpdateCursor(mainPanelButtons[mainButtonIndex]);
 
         yield return new WaitForSeconds(mainPanelButtonScaleDuration);
         activeUIAction = null;
         yield return null;
     }
+
+    private void InitCursor() {
+        if (mainButtonIndex == -1) {
+            cursor.DOScale(new Vector3(1, 1, 1), mainPanelButtonScaleDuration).SetEase(Ease.Flash);
+            cursor.gameObject.SetActive(true);
+        }
+    }
+
+    private void CollapseCursor() {
+        cursor.DOScale(new Vector3(0, 0, 0), mainPanelButtonScaleDuration);
+    }
+
+    private void UpdateCursor(BattleButton button) {
+        cursor.DOMove(button.targetPoint.position, mainPanelButtonScaleDuration);
+    }
+    
     public void ActivateMainPanelButton() {
         mainPanelButtons[mainButtonIndex].Scale(new Vector3(1, 1, 1), mainPanelButtonScaleDuration);
         mainPanelButtons[mainButtonIndex].Activate(_stateMachine, mainPanelButtonScaleDuration);
