c074a476713b943ef2b8a676a2c02167d286a40b
clee857 <chrysly0816@gmail.com>
2023-11-05 22:08:04 -0500

    Share button -> end turn prompt

--- Assets/_Scripts/Turn Based Mechanics/UI/Buttons/ShareButton.cs
+++ Assets/_Scripts/Turn Based Mechanics/UI/Buttons/ShareButton.cs
@@ -7,6 +7,7 @@ public class ShareButton : BattleButton
     public override void Activate(BattleUIStateMachine stateMachine, float delay) {
         base.Activate(stateMachine, delay);
         
-        stateMachine.DelayedTransition<BattleUIStateMachine.InitUIState>(0.2f, true);
+        stateMachine.DelayedTransition<BattleUIStateMachine.BattleUI_Limbo>(delay, false);
+        stateMachine._battleStateMachine.StartBattle(0.5f);
     }
 }
