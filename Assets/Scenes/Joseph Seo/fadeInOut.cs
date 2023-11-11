7b546537ae5c925cb69a6fccc0be58a408c93226
Joseph Seo <134018750+seojoseph4@users.noreply.github.com>
2023-11-11 17:38:10 -0500

    CustomizableFade

--- Assets/Scenes/Joseph Seo/fadeInOut.cs
+++ Assets/Scenes/Joseph Seo/fadeInOut.cs
@@ -8,21 +8,28 @@ public class FadeInOut : MonoBehaviour
 {
     [SerializeField] private float transitionRate=0.03f;
     [SerializeField] public CanvasGroup myCanvasGroup;
-    [SerializeField] private GameObject loadingCanvas;
+    [SerializeField] private GameObject transitionCanvas;
+    [SerializeField] private GameObject loadingComponents;
     private float fadeValues;
+    private Color _color;
 
 
-    public void Fade(float goTo){
+    public void Fade(float goTo,bool load,Color? color=null){
+        _color = color ?? Color.black;
         fadeValues = goTo;
-        StartCoroutine(_Fade(fadeValues));
+        StartCoroutine(_Fade(fadeValues,load,_color));
     }
 
-    IEnumerator _Fade(float goTo){
+    public void Fade(float goTo, bool load) => Fade(goTo,load,Color.black);
+
+    IEnumerator _Fade(float goTo,bool load,Color color){
+        transitionCanvas.GetComponent<Image>().color=color;
+        loadingComponents.SetActive(load);
         while(myCanvasGroup.alpha!=goTo){
             myCanvasGroup.alpha = Mathf.MoveTowards(myCanvasGroup.alpha,goTo,transitionRate*Time.deltaTime);
             if(myCanvasGroup.alpha==goTo){
                 if(myCanvasGroup.alpha<0.5f){
-                    loadingCanvas.SetActive(false);
+                    transitionCanvas.SetActive(false);
                 }
             }
             yield return null;
