039f65090c529acc1460a0a70b2ade0fa1dce193
Carlijavier5 <carlijavier5@gmail.com>
2023-11-29 01:20:22 -0500

    Reorganizing and splitting UI classes

--- Assets/_Scripts/Turn Based Mechanics/UI New/Identifiers/UICursorHandler.cs
+++ Assets/_Scripts/Turn Based Mechanics/UI New/Identifiers/UICursorHandler.cs
@@ -2,6 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SkillNameIdentifier : MonoBehaviour
-{
+public abstract class UICursorHandler : MonoBehaviour {
+
 }
